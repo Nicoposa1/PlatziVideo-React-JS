0c65d446f3f0979919c0112194f45500f6cac647
Nicolas Posa <nicoposa57@gmail.com>
2020-07-14 14:50:50 -0300

    Footer y agregando las imagenes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem'
+import Footer from '../components/Footer'
 import '../assets/styles/App.scss';
 
 const App = () => (
@@ -21,8 +22,12 @@ const App = () => (
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
       </Carousel>
     </Categories>
+    <Footer />
   </div>
 )
 
