73de37c146f557e5c7d7b77f2e100202a9f44d18
Nicolas Posa <nicoposa57@gmail.com>
2020-07-14 20:46:49 -0300

    hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,56 +8,35 @@ import Footer from '../components/Footer'
 import '../assets/styles/App.scss';
 
 const App = () => {
-  const [ videos, setVideos ] = useState([]);
-
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
   useEffect(() => {
     fetch('http://localhost:3000/initalState')
       .then(response => response.json())
       .then(data => setVideos(data));
-  }, [])
-
-  console.log(videos)
+  }, []);
 
   return (
     <div className='App'>
       <Header />
       <Search />
-
-      <Categories title='Mi Lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
       <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {vide.trends.map(item =>
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
       <Categories title='Originales de PlatziVideo'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
           <CarouselItem />
         </Carousel>
       </Categories>
