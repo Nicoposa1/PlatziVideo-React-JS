0b0dad2b1195775ac84bda5775302cf619efa3e7
Nicolas Posa <nicoposa57@gmail.com>
2020-07-21 18:00:54 -0300

    Agregue 404 Not Found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/styles/components/NotFound.scss'
+
+const NotFound = () => (
+  <div className="error">
+    <div className="error__container">
+        <h1>Error 404</h1>
+        <p>Pagina no encontrada</p>
+    </div>
+  </div>
+)
+
+export default NotFound;
