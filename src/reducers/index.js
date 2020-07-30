c37e95f6eac31d04c4dfb6720ecba2736b4e7c76
Nicolas Posa <nicoposa57@gmail.com>
2020-07-29 22:03:43 -0300

    creando logica para el login con redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_FAVORITE':
+      const exist = state.myList.find((item) => item.id === action.payload.id)
+      if (exist) return { ...state }
+
       return {
         ...state,
         myList: [...state.myList, action.payload],
@@ -8,7 +11,12 @@ const reducer = (state, action) => {
     case 'DELETE_FAVORITE':
       return {
         ...state,
-        myList: state.myList.filter(items => items.id !== action.payload)
+        myList: state.myList.filter(items => items.id !== action.payload),
+      }
+    case 'LOGIN_REQUEST':
+      return {
+        ...state,
+        user:action.payload,
       }
     default:
       return state;
